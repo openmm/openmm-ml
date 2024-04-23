b7ab9ed12673138f1edf7737af07392bbaf2cf80
Joao Morado <joaomorado@gmail.com>
2024-04-23 11:55:11 +0100

    Updated examples

--- examples/nequip/README.md
+++ examples/nequip/README.md
@@ -4,14 +4,13 @@ This directory contains examples for running simulations using a NequIP potentia
 
 ## Installation
 
-
-first install openmm-torch from conda-forge:
+First install the `openmm-torch` and `nnpops` packages from conda-forge:
 
 ```
-conda install -c conda-forge openmm-torch
+conda install -c conda-forge openmm-torch nnpops
 ```
 
-Then install NequIP development branch and this version of openmm-ml using pip
+Then install NequIP development branch and this version of `openmm-ml` using pip:
 
 ```
 pip install git+https://github.com/mir-group/nequip@develop
@@ -25,18 +24,19 @@ Once you have a deployed trained NequIP model you can use it as the potential in
 ```python
 from openmmml import MLPotential
 
-# create a System with NequIP MLP
+# Create a System with NequIP MLP
 
-# need to specify the unit conversion factors from the NequIP model units to OpenMM units.
+# Need to specify the unit conversion factors from the NequIP model units to OpenMM units.
 # e.g.:
-# distance: model is in Angstrom, OpenMM is in nanometers
+# Distance: model is in Angstrom, OpenMM is in nanometers
 A_to_nm = 0.1
-# energy: model is in kcal/mol, OpenMM is in kJ/mol
+# Energy: model is in kcal/mol, OpenMM is in kJ/mol
 kcal_to_kJ_per_mol = 4.184
 
-potential = MLPotential('nequip', model_path='example_model_deployed.pth',
-                        distance_to_nm=A_to_nm,
-                        energy_to_kJ_per_mol=kcal_to_kJ_per_mol)
+potential = MLPotential('nequip', 
+                        modelPath='example_model_deployed.pth',
+                        lengthScale=A_to_nm,
+                        energyScale=kcal_to_kJ_per_mol)
 
 system = potential.createSystem(topology)
 ```
@@ -44,4 +44,4 @@ system = potential.createSystem(topology)
 ## Example
 
 ### run_nequip.ipynb
-Runs a simulation using the model created by NequIP example [config/example.yaml](https://github.com/mir-group/nequip/blob/main/configs/example.yaml). It is available as a python script: [`run_nequip.py`](run_nequip.py) and a Jupyter notebook [`run_nequip.ipynb`](run_nequip.ipynb) which can be run on Colab.
+Runs a simulation using the model created by NequIP example [config/example.yaml](https://github.com/mir-group/nequip/blob/main/configs/example.yaml). It is available as a Python script: [`run_nequip.py`](run_nequip.py) and a Jupyter notebook [`run_nequip.ipynb`](run_nequip.ipynb) which can be run on Colab.
--- examples/nequip/run_nequip.py
+++ examples/nequip/run_nequip.py
@@ -1,8 +1,10 @@
+from sys import stdout
+
 import openmm
 import openmm.app as app
 import openmm.unit as unit
+
 from openmmml import MLPotential
-from sys import stdout
 
 """
 Uses a deployed trained NequIP model from the basic example on https://github.com/mir-group/nequip
@@ -10,34 +12,42 @@ Uses a deployed trained NequIP model from the basic example on https://github.co
 >>> nequip-deploy build --train-dir path/to/training/session/ example_model_deployed.pth
 """
 
-# load toluene structure
-pdb = app.PDBFile('toluene.pdb')
+# Load toluene structure
+pdb = app.PDBFile("toluene.pdb")
 
-# create a System with NequIP MLP
+# Create a System with NequIP MLP
 
-# need to specify the unit conversion factors from the NequIP model units to OpenMM units.
-# distance: model is in Angstrom, OpenMM is in nanometers
+# Need to specify the unit conversion factors from the NequIP model units to OpenMM units.
+# Distance: model is in Angstrom, OpenMM is in nanometers
 A_to_nm = 0.1
-# energy: model is in kcal/mol, OpenMM is in kJ/mol
+# Energy: model is in kcal/mol, OpenMM is in kJ/mol
 kcal_to_kJ_per_mol = 4.184
 
-potential = MLPotential('nequip', model_path='example_model_deployed.pth',
-                        distance_to_nm=A_to_nm,
-                        energy_to_kJ_per_mol=kcal_to_kJ_per_mol)
+potential = MLPotential(
+    "nequip",
+    modelPath="example_model_deployed.pth",
+    lengthScale=A_to_nm,
+    energyScale=kcal_to_kJ_per_mol,
+)
 
 system = potential.createSystem(pdb.topology)
 
-# run langevin dynamics at 300K for 1000 steps
-integrator = openmm.LangevinIntegrator(300*unit.kelvin, 10.0/unit.picoseconds, 1.0*unit.femtosecond)
+# Run langevin dynamics at 300K for 1000 steps
+integrator = openmm.LangevinIntegrator(
+    300 * unit.kelvin, 10.0 / unit.picoseconds, 1.0 * unit.femtosecond
+)
 simulation = app.Simulation(pdb.topology, system, integrator)
 simulation.context.setPositions(pdb.positions)
-simulation.reporters.append(app.PDBReporter('output.pdb', 100))
-simulation.reporters.append(app.StateDataReporter(stdout, 100, step=True,
-        potentialEnergy=True, temperature=True, speed=True))
+simulation.reporters.append(app.PDBReporter("output.pdb", 100))
+simulation.reporters.append(
+    app.StateDataReporter(
+        stdout, 100, step=True, potentialEnergy=True, temperature=True, speed=True
+    )
+)
 
 simulation.step(1000)
 
 # Minimize the energy
 simulation.minimizeEnergy()
-energy=simulation.context.getState(getEnergy=True).getPotentialEnergy()
+energy = simulation.context.getState(getEnergy=True).getPotentialEnergy()
 print(energy)
