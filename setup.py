69f6ea1296d8230c7739fd894a99f464f75c9971
peastman <peastman@stanford.edu>
2024-06-06 08:30:32 -0700

    Updated version number to 1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 DOCLINES = __doc__.split("\n")
 
 ########################
-__version__ = '1.1'
+__version__ = '1.2'
 VERSION = __version__
 ISRELEASED = False
 ########################
