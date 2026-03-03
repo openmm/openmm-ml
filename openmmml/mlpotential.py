741781af452736144ffd691429ce4e196d67ab79
Evan Pretti <pretti@stanford.edu>
2026-03-03 11:38:31 -0800

    Update header year

--- openmmml/mlpotential.py
+++ openmmml/mlpotential.py
@@ -6,7 +6,7 @@ Simbios, the NIH National Center for Physics-Based Simulation of
 Biological Structures at Stanford, funded under the NIH Roadmap for
 Medical Research, grant U54 GM072970. See https://simtk.org.
 
-Portions copyright (c) 2021-2024 Stanford University and the Authors.
+Portions copyright (c) 2021-2026 Stanford University and the Authors.
 Authors: Peter Eastman
 Contributors:
 
