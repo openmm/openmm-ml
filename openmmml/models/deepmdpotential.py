c1183371515b8b8de793d5d2324166b982d492f3
Peter Eastman <peastman@stanford.edu>
2026-04-28 08:31:12 -0700

    Pass args to DeepmdPotential (#144)

--- openmmml/models/deepmdpotential.py
+++ openmmml/models/deepmdpotential.py
@@ -6,7 +6,7 @@ Simbios, the NIH National Center for Physics-Based Simulation of
 Biological Structures at Stanford, funded under the NIH Roadmap for
 Medical Research, grant U54 GM072970. See https://simtk.org.
 
-Portions copyright (c) 2021-2023 Stanford University and the Authors.
+Portions copyright (c) 2021-2026 Stanford University and the Authors.
 Authors: Peter Eastman
 Contributors:
 
@@ -37,7 +37,7 @@ class DeepmdPotentialImplFactory(MLPotentialImplFactory):
     """This is the factory that creates DeepmdPotentialImpl objects."""
 
     def createImpl(self, name: str, **args) -> MLPotentialImpl:
-        return DeepmdPotentialImpl(name)
+        return DeepmdPotentialImpl(name, **args)
 
 
 class DeepmdPotentialImpl(MLPotentialImpl):
