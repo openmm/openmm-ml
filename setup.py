51fcb673d9e4847c71987c7bbe42b86c25f92edc
Peter Eastman <peastman@stanford.edu>
2026-09-08 13:01:02 -0700

    Require OpenMM 8.6.1

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     classifiers=CLASSIFIERS.splitlines(),
     packages=find_packages(),
     zip_safe=False,
-    install_requires=['numpy', 'openmm >= 8.6.0'],
+    install_requires=['numpy', 'openmm >= 8.6.1'],
     entry_points={
         'openmmml.potentials': [
             # Custom AIMNet2 model supplied by path.
